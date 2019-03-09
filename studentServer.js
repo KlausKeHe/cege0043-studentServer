ad25471016b8e57f2c10da6424a96b5bb9307156
KlausKeHe <uceskh0@ucl.ac.uk>
2019-03-09 11:13:49 -0500

    add the code that request for data from this server via another server

--- studentServer.js
+++ studentServer.js
@@ -24,6 +24,14 @@ app.use(function(req,res,next){
 	next();
 });
 
+var app = express();
+
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","X-Requested-With");
+	next();
+});
+
 //app.get('/test.html',function(req,res){
 	//run some server-side code
 	//console.log('test.html requested');
